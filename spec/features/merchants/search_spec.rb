c795a159424ccffc74dc3f8ebf4b310d58a26507
Nathan Brown <nathan.brown263@gmail.com>
2021-12-13 16:37:25 -0700

    feat: Working search bar (with test)

--- spec/features/merchants/search_spec.rb
+++ spec/features/merchants/search_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants search page' do
   before(:each) do
-    visit '/merchants/search'
+    visit '/merchants'
   end
   it 'has a merchant search bar' do
-    fill_in 'search', with: 'iLl'
-
-    click_button 'Search'
+    fill_in with: 'iLl'
+    
+    click_button 'Submit'
 
     expect(current_path).to eq('/merchants/search')
-    
+
     expect(page).to have_content('Willms and Sons')
     expect(page).to have_content('Schiller, Barrows and Parker')
   end
