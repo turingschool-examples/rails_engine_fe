16b8ef0fa46d766004b4767ee9cbb8308b3940f0
Chaz Simons <chaz.simons@gmail.com>
2021-12-13 13:28:57 -0800

    Add additional test for merchant service

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe 'Merchant Service' do
     expect(merchants).to be_a(Array)
     expect(merchants.count).to eq(100)
   end
+
+  it 'can return a single merchant' do
+    merchant = MerchantService.one_merchant(1)[:data]
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes][:name]).to be_a(String)
+  end
+
+  it 'can find a merchants items' do
+    merchant_items = MerchantService.merchant_items(1)
+    data = merchant_items[:data]
+
+    expect(merchant_items).to be_a(Hash)
+    expect(data).to be_a(Array)
+    expect(data.count).to eq(15)
+
+    data.each do |item|
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
 end
