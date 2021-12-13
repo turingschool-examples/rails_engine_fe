d15adb3eacc98be9c0997750ed7657c0c61a7a86
Micha Bernhard <mib70@pitt.edu>
2021-12-13 15:40:42 -0700

    Test: Merchant Index

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -1,23 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Index Page' do
-  let!(:merchant1) {Merchant.create!(name: 'Merchant1')}
-  let!(:merchant2) {Merchant.create!(name: 'Merchant2')}
-  let!(:merchant3) {Merchant.create!(name: 'Merchant3')}
-
   it 'displays all merchants' do
-    visit merchants_index_path
+    visit merchants_path
 
-    expect(page).to have_link('Merchant1')
-    expect(page).to have_link('Merchant2')
-    expect(page).to have_link('Merchant3')
+    expect(page).to have_link('Schroeder-Jerde')
+    expect(page).to have_link('Glover Inc')
   end
 
   it 'links to the merchants show page' do
     visit merchants_path
+    merchant = MerchantFacade.merchants_index.first
+    click_link 'Schroeder-Jerde'
 
-    click_link 'Merchant1'
-
-    expect(current_path).to eq(merchants_path(merchant1))
+    expect(current_path).to eq("/merchants/#{merchant.id}")
   end
 end
