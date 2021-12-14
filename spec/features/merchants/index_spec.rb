29c28c852175c7e07e8671abc30ce274f9c71505
Jacob Yarborough <jayarborough95@gmail.com>
2021-12-13 20:21:36 -0700

    Create merchant index test

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'merchants show page' do 
+  describe 'happy path' do 
+    it 'shows list of merchants' do 
+      VCR.use_cassette('rails-engine_merchants', re_record_interval: 7.days) do 
+        visit "/merchants"
+     
+        expect(page.status_code).to eq(200)
+
+        expect(page).to have_link("Schroeder-Jerde")
+      end 
+    end 
+  end 
+end 
