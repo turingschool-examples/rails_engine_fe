bb82225a9ff83042a094c277235da224ed883cef
Jacob Yarborough <jayarborough95@gmail.com>
2021-12-13 20:28:56 -0700

    Add merchant show testing

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,3 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe ''
+RSpec.describe 'merchant show page' do 
+  describe 'happy path' do 
+    it 'shows merchant and items' do 
+      VCR.use_cassette('rails-engine_merchant_show', re_record_interval: 7.days) do 
+        visit '/merchants/1' 
+
+        expect(page.status_code).to eq(200)
+        expect(page).to have_content("Schroeder-Jerde")
+        expect(page).to have_link('Item Nemo Facere')
+      end 
+    end 
+  end 
+end 
