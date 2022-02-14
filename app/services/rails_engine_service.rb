9e94ab8b34c3afa4cf768e04b9c60c032a609c7b
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-14 17:20:11 -0600

    service with merchant and item call methods

--- app/services/rails_engine_service.rb
+++ app/services/rails_engine_service.rb
@@ -6,6 +6,18 @@ class RailsEngineService
     get_url("/merchants")
   end
 
+  def self.a_merchant(merchant_id)
+    get_url("/merchants/#{merchant_id}")
+  end
+
+  def self.merchant_items(merchant_id)
+    get_url("/merchants/#{merchant_id}/items")
+  end
+
+  def self.an_item(item_id)
+    get_url("/items/#{item_id}")
+  end
+
   def self.get_url(url)
     response = Faraday.get("http://localhost:3000/api/v1#{url}")
     parsed = JSON.parse(response.body, symbolize_names: true)
