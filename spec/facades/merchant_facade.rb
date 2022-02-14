ea4138ae872daafa950619da67736598bfa54acb
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-14 14:24:58 -0700

    Create tests for merchant#find_merchant(id) and #find_merchant_items(id)

--- spec/facades/merchant_facade.rb
+++ spec/facades/merchant_facade.rb
@@ -5,10 +5,34 @@ RSpec.describe MerchantFacade do
     context '#all_merchants' do
       it 'returns Merchant data', :vcr do
         merchant = MerchantFacade.new
-        expect(merchant.all_merchants).to be_a(Array)
+        merchants = merchant.all_merchants
+        expect(merchants).to be_a(Array)
 
-        merchant.all_merchants.each do |merchant|
+        merchants.each do |merchant|
           expect(merchant).to be_a(Merchant)
+        end
+      end
+    end
+
+    context '#find_merchant(id)' do
+      it 'returns a single merchants data', :vcr do
+        merchants = MerchantFacade.new
+        merchant = merchants.find_merchant(1)
+
+        expect(merchant).to be_a(Merchant)
+        expect(merchant.id).to be_a(Integer)
+        expect(merchant.name).to be_a(String)
+      end
+    end
+
+    context '#find_merchant_items(id)', :vcr do
+      it 'returns item data' do
+        merchant = MerchantFacade.new
+        items = merchant.find_merchant_items(1)
+        expect(items).to be_a(Array)
+
+        items.each do |item|
+          expect(item).to be_a(Item)
         end
       end
     end
