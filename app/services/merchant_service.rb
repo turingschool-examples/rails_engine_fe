43a80d8f159308fd31de7105eb8bf360ed4f0f25
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 10:24:06 -0800

    Add all merchants method to merchant facade.

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,2 +1,10 @@
 class MerchantService
+  def all_merchants
+    response = conn.get("/api/v1/merchants")
+    JSON.parse(response.body, symbolize_names: true)[:data]
+  end
+
+  def conn
+    Faraday.new("https://localhost:3000") do |faraday|
+  end
 end
