e3c0fc9edd6c9daf2db19f2c82b2307479c8424a
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 10:24:33 -0800

    Turn instance methods to class methods.

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,10 +1,10 @@
 class MerchantService
-  def all_merchants
+  def self.all_merchants
     response = conn.get("/api/v1/merchants")
     JSON.parse(response.body, symbolize_names: true)[:data]
   end
 
-  def conn
+  def self.conn
     Faraday.new("https://localhost:3000") do |faraday|
   end
 end
