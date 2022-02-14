188f63025d13c3fd56cb57d0468157f7bc7e283a
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 15:09:45 -0800

    Refactor index page view.

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,20 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Index Page' do
   describe 'View' do
-    it 'has a title and links to merchants' do
-      # Faraday.new('http:///api/v1/merchants')
-
-      visit '/merchants'
+    it 'has a title and links to merchants', :vcr do
+      visit merchants_path
 
       expect(page).to have_content("Merchants")
-      expect(page).to have_link(merchant_1.name)
-      expect(page).to have_link(merchant_2.name)
-      expect(page).to have_link(merchant_3.name)
 
-      click_link("#{merchant_1.name}")
-
-      expect(current_path).to eq("/merchants/#{merchant_1.id}")
-      expect(page).to have_content(merchant_1.items.first.name)
+      click_link("Schroeder-Jerde")
+      expect(current_path).to eq("/merchants/1")
+      expect(page).to have_content(merchant.items.first.name)
     end
   end
 end
