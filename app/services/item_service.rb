4d8d1e25be60eaeb1c9078eacc60353ddec9cdf6
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 15:23:42 -0800

    Create item service file.

--- app/services/item_service.rb
+++ app/services/item_service.rb
@@ -1,2 +1,10 @@
 class ItemService
+  def self.all_items
+    response = conn.get("/api/v1/items")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new("http://localhost:3000")
+  end
 end
