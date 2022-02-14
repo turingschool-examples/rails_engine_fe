0dceed8cfc394fa11c71fdbb9d7fe6bf505909e4
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-14 17:19:36 -0600

    Merchant controller show action

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,4 +3,9 @@ class MerchantsController < ApplicationController
   def index
     @merchants = RailsEngineFacade.merchant_list
   end
-end 
+
+  def show
+    @merchant = RailsEngineFacade.one_merchant(params[:id])
+    @items = RailsEngineFacade.merchant_item_list(params[:id])
+  end 
+end
