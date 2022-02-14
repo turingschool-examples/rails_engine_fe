2c6d5e496cbff52a624b9d2a64fa1568c85b378c
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 14:59:31 -0800

    Inheret application controller.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,8 +1,10 @@
-class MerchantsController
+class MerchantsController < ApplicationController
   def index
     @merchants = MerchantFacade.all_merchants
   end
 
   def show
+    @merchant = MerchantFacade.merchant_info(params[:id])
+    @items = MerchantFacade.merchant_items(params[:id])
   end
 end
