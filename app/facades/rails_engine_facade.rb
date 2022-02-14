a90fee7ecd3d60a1044cdf013a0bfa11d6a5b06c
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-14 17:20:34 -0600

    facade with merchant and item methods

--- app/facades/rails_engine_facade.rb
+++ app/facades/rails_engine_facade.rb
@@ -5,4 +5,19 @@ class RailsEngineFacade
       Merchant.new(data)
     end
   end
-end 
+
+  def self.one_merchant(merchant_id)
+    Merchant.new(RailsEngineService.a_merchant(merchant_id)[:data])
+  end
+
+  def self.merchant_item_list(merchant_id)
+    RailsEngineService.merchant_items(merchant_id)[:data].map do |data|
+      Item.new(data)
+    end
+  end
+
+  def self.one_item(item_id)
+    Item.new(RailsEngineService.an_item(item_id)[:data])
+  end
+
+end
