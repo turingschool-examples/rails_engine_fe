817b3ce6285d46954d1df47761b0ce5cef30e903
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 15:00:00 -0800

    Redefine variables.

--- app/poros/item.rb
+++ app/poros/item.rb
@@ -1,9 +1,11 @@
 class Item
-  attr_reader :id, :name, :description, :unit_price
+  attr_reader :id, :name, :description, :unit_price, :merchant_id
+
   def initialize(data)
     @id = data[:id]
-    @name = data[:name]
-    @description = data[:description]
-    @unit_price = data[:unit_price]
+    @name = data[:attributes][:name]
+    @description = data[:attributes][:description]
+    @unit_price = data[:attributes][:unit_price]
+    @merchant_id = data[:attributes][:merchant_id]
   end
 end
