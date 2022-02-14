db5d65844c4129306e67183017527dc538a54d9a
Joel Grant <joelmgrant@me.com>
2022-02-14 14:54:40 -0700

    Comment out code and add note to instructors

--- spec/features/welcome/search_spec.rb
+++ spec/features/welcome/search_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Home/Search Page' do
   describe 'when I land on the root page' do
+    # I realized I never implemented this for all merchants in the other project :D
     xit 'has a box where I can search by merchant' do
       visit "/"
 
