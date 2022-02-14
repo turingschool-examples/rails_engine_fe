381c218b9e7866d371ee73f9e7f847e452319341
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 14:59:47 -0800

    Redifine variables.

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,9 +1,20 @@
 class MerchantFacade
   def self.all_merchants
     json = MerchantService.all_merchants
-
-    json.map do |merchant|
+    json[:data].map do |merchant|
       Merchant.new(merchant)
+    end
+  end
+
+  def self.merchant_info(merchant_id)
+    json = MerchantService.merchant_info(merchant_id)
+    Merchant.new(json[:data])
+  end
+
+  def self.merchant_items(merchant_id)
+    json = MerchantService.merchant_items(merchant_id)
+    json.map do |item|
+      Item.new(item)
     end
   end
 end
