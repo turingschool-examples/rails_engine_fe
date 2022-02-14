99ea407222b77df0ab643424325c233c2ed7a784
Robin Dittrich <robinodittrich@gmail.com>
2022-02-14 15:10:48 -0800

    Add all merchants , merchant info and merchant items methods to service.

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,10 +1,20 @@
 class MerchantService
   def self.all_merchants
     response = conn.get("/api/v1/merchants")
-    JSON.parse(response.body, symbolize_names: true)[:data]
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.merchant_info(merchant_id)
+    response = conn.get("/api/v1/merchants/#{merchant_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.merchant_items(merchant_id)
+    response = conn.get("/api/v1/merchants/#{merchant_id}/items")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
-    Faraday.new("https://localhost:3000") do |faraday|
+    Faraday.new("http://localhost:3000")
   end
 end
