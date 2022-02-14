ed5f7ed0b9dbbfc252fa6abe51b9f4e345f01152
Darren Kulback <dkulback@gmail.com>
2022-02-14 10:31:39 -0700

    Tests and set up gem

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants Index' do
+  it 'displays all the merchants' do
+    VCR.use_cassette('merchants') do
+      visit merchants_path
+
+      within '.merchants' do
+        expect(page).to have_content('Schroeder-Jerde')
+        expect(page).to have_content('Klein, Rempel and Jones')
+
+        click_link 'Schroeder-Jerde'
+
+        expect(current_path).to eq(merchant_path(2))
+      end
+    end
+  end
+end
