74bb22f64f4e2c870f225199fa9cd7d91e3037b6
Joel Grant <joelmgrant@me.com>
2022-02-14 14:36:36 -0700

    Add items and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get '/merchants', to: 'merchants#index'
+  get '/', to: 'home#index'
+  resources :merchants, only: [:index, :show]
+  resources :items, only: [:index, :show]
 end
