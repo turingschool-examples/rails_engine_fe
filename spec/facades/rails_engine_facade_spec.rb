a15bc103b8e4942a52f45c0ce0094a89ccc65b00
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-14 18:04:15 -0600

    Facade tests item methods

--- spec/facades/rails_engine_facade_spec.rb
+++ spec/facades/rails_engine_facade_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe RailsEngineFacade do
       expect(merchants.first).to be_instance_of(Merchant)
     end
   end
+
+  describe 'one_merchant' do
+    it 'returns merchant by merchant_id' do
+      merchant = RailsEngineFacade.one_merchant(1)
+
+      expect(merchant).to be_instance_of(Merchant)
+    end
+  end
+
+  describe 'merchant_item_list' do
+    it 'returns merchant item object array' do
+      merchant_items = RailsEngineFacade.merchant_item_list(1)
+
+      expect(merchant_items).to be_an(Array)
+      expect(merchant_items.first).to be_instance_of(Item)
+    end
+  end
+
+  describe 'one_item' do
+    it 'returns item by item id' do
+      item = RailsEngineFacade.one_item(4)
+
+      expect(item).to be_instance_of(Item)
+    end
+  end 
 end
