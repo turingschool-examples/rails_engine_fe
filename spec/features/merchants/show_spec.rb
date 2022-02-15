11f894d364594d9803bdd1ff7b1039836be185f7
Mallory Vining <mallorygallery12@gmail.com>
2022-02-14 18:41:48 -0700

    Add Show page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants Show Page" do
-  xit "displays a merchant by id" do
-    visit (merchant_path(id))
+  it "displays a merchant and their items by id" do
+    visit (merchant_path(4))
 
-    expect(page).to have_content(merchants name)
-    expect(page).to have_content(merchants items)
+    expect(page).to have_content("Cummings-Thiel")
+    expect(page).to have_content("Item In Sed")
+    expect(page).to have_content("Item Voluptate Quo")
   end
 end
