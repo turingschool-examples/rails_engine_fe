f453d0ec2ef2dc2285540207490bd1bd7c1545b2
Seth Perna <gseth26@gmail.com>
2022-02-16 09:27:23 -0500

    add faraday service

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,13 +1,4 @@
-class MerchantService
-  def self.get_url(url, query = nil)
-    conn = Faraday.new(url: 'http://localhost:3000') do |faraday|
-      faraday.params[:query] = query unless query.nil?
-    end
-
-    response = conn.get(url)
-
-    data = JSON.parse(response.body, symbolize_names: true)
-  end
+class MerchantService < FaradayService
 
   def self.get_merchants
     get_url('/api/v1/merchants')
