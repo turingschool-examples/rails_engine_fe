c085e4f0c22bfe998f0c47eb801703bd62fd1b0a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 15:19:50 -0500

    Refactor: move expects into each block

--- spec/services/item_service_spec.rb
+++ spec/services/item_service_spec.rb
@@ -8,22 +8,23 @@ RSpec.describe ItemService do
         expect(search).to be_a Hash
 
         expect(search[:data]).to be_an Array
-        item_data = search[:data].first
 
-        expect(item_data).to have_key(:id)
-        expect(item_data[:id]).to be_a(String)
-        
-        expect(item_data[:attributes]).to have_key(:merchant_id)
-        expect(item_data[:attributes][:merchant_id]).to be_a(Integer)
-
-        expect(item_data[:attributes]).to have_key(:name)
-        expect(item_data[:attributes][:name]).to be_a(String)
-
-        expect(item_data[:attributes]).to have_key(:description)
-        expect(item_data[:attributes][:description]).to be_a(String)
-
-        expect(item_data[:attributes]).to have_key(:unit_price)
-        expect(item_data[:attributes][:unit_price]).to be_a(Float)
+        search[:data].each do |item_data|
+          expect(item_data).to have_key(:id)
+          expect(item_data[:id]).to be_a(String)
+          
+          expect(item_data[:attributes]).to have_key(:merchant_id)
+          expect(item_data[:attributes][:merchant_id]).to be_a(Integer)
+  
+          expect(item_data[:attributes]).to have_key(:name)
+          expect(item_data[:attributes][:name]).to be_a(String)
+  
+          expect(item_data[:attributes]).to have_key(:description)
+          expect(item_data[:attributes][:description]).to be_a(String)
+  
+          expect(item_data[:attributes]).to have_key(:unit_price)
+          expect(item_data[:attributes][:unit_price]).to be_a(Float)
+        end
       end
     end
     context '#item_info(id)' do 
