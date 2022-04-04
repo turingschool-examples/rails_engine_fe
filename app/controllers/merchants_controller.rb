601e94206275defbbfe4c8767bb91249e3907646
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 13:26:31 -0500

    Feat: Merchant#show returns merchant and merchant_items from facade

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,5 +4,8 @@ class MerchantsController < ApplicationController
     @merchants = MerchantFacade.all_merchants
   end
 
-
+  def show
+    @merchant = MerchantFacade.merchant_info(params[:id])
+    @items = MerchantFacade.merchant_items(params[:id])
+  end
 end
