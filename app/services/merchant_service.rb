30e227fe3c79e56fd8c8270dfa28b25d1ac4457b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 12:12:08 -0500

    Add MerchantService to hit merchant endpoints

---
+++ app/services/merchant_service.rb
@@ -0,0 +1,17 @@
+class MerchantService 
+  def self.conn
+    Faraday.new(url: 'http://localhost:3000/api/v1') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def self.all_merchants
+    response = conn.get('merchants')
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.merchant_info(id)
+    response = conn.get("merchants/#{id}")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+end
