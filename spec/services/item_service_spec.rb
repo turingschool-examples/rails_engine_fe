8cd71b2207d0f7048724fe15410d01780ac1d683
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 15:05:52 -0500

    Test: ItemService#all_items returns a list of all items

---
+++ spec/services/item_service_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe ItemService do
+  context 'class methods' do
+    context '#all_items' do 
+      it 'returns a list of all items' do 
+        search = ItemService.all_items
+        expect(search).to be_a Hash
+
+        expect(search[:data]).to be_an Array
+        item_data = search[:data].first
+
+        expect(item_data).to have_key(:id)
+        expect(item_data[:id]).to be_a(String)
+        
+        expect(item_data[:attributes]).to have_key(:merchant_id)
+        expect(item_data[:attributes][:merchant_id]).to be_a(Integer)
+
+        expect(item_data[:attributes]).to have_key(:name)
+        expect(item_data[:attributes][:name]).to be_a(String)
+
+        expect(item_data[:attributes]).to have_key(:description)
+        expect(item_data[:attributes][:description]).to be_a(String)
+
+        expect(item_data[:attributes]).to have_key(:unit_price)
+        expect(item_data[:attributes][:unit_price]).to be_a(Float)
+
+      end
+    end
+    context '#item_info(id)' do 
+      xit 'returns data for a single item' do 
+
+      end
+    end
+    context '#item_merchant(item_id)' do 
+      xit 'returns the merchant info for a single item' do 
+
+      end
+    end
+  end
+end 
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe MerchantService do
-  context 'class methods' do
-    
+   context 'class methods' do
+    context '#all_merchants' do 
+      xit 'returns a list of all merchants' do 
+
+      end
+    end
+    context '#merchant_info(id)' do 
+      xit 'returns data for a single merchant' do 
+
+      end
+    end
+    context '#merchant_items(merchant_id)' do 
+      xit 'returns a list of all items for a single merchant' do 
+
+      end
+    end
   end
 end 
