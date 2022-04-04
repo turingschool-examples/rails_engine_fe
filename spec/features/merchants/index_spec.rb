34305ae7f5df247d4e3233f7a55b328b324193b4
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 13:10:00 -0500

    Test: each merchant name is a link to that merchants show page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 RSpec.describe 'Movie Index Page' do
   it 'has list of all merchants' do 
     visit merchants_path
-
+    merchants = MerchantFacade.all_merchants
+    
     expect(page).to have_css("#merchants")
-
+    expect(page).to have_css("#name, merchants.count")
+    
     within("#merchants") do
       expect(page).to have_css("#name")
     end
   end
-
-  xit 'each merchant name is a link to merchant show page' do 
+  
+  it 'each merchant name is a link to merchant show page' do 
     visit merchants_path
+    first_merchant = MerchantFacade.all_merchants.first
+
+    within(first("#name")) do 
+      click_link
+    end
+
+    expect(current_path).to eq(merchant_path(first_merchant.id))
   end
-
-
 end
