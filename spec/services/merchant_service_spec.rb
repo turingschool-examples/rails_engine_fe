597a0875cdbfa30656b03705d1fb2e9689922004
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 15:20:35 -0500

    Refactor: move expects into each block

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -8,18 +8,30 @@ RSpec.describe MerchantService do
         expect(search).to be_a Hash
 
         expect(search[:data]).to be_an Array
-        merchant_data = search[:data].first
+
+        search[:data].each do |merchant_data|
+          expect(merchant_data).to have_key(:id)
+          expect(merchant_data[:id]).to be_a(String)
+          
+          expect(merchant_data[:attributes]).to have_key(:name)
+          expect(merchant_data[:attributes][:name]).to be_a(String)
+        end
+      end
+    end
+    context '#merchant_info(id)' do 
+      xit 'returns data for a single merchant' do 
+        search = MerchantService.all_merchants
+        expect(search).to be_a Hash
+
+        expect(search.count).to eq(1)
+
+        merchant_data = search[:data]
 
         expect(merchant_data).to have_key(:id)
         expect(merchant_data[:id]).to be_a(String)
         
         expect(merchant_data[:attributes]).to have_key(:name)
         expect(merchant_data[:attributes][:name]).to be_a(String)
-      end
-    end
-    context '#merchant_info(id)' do 
-      xit 'returns data for a single merchant' do 
-
       end
     end
     context '#merchant_items(merchant_id)' do 
