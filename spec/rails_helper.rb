46154525a216aea9255c3db926a5eaec9d7febc2
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-04 13:15:11 -0500

    update gem for local 5000 to connect

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,8 +67,8 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
-end
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-end
+ends
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+#   config.hook_into :webmock
+# end
