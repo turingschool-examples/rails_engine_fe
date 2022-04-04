82189b98e7209fd357bc8f8f167ec99e6402b89c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 16:00:20 -0500

    merchant_items creates Item objects for all items returned that belong to Merchant

--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -34,5 +34,20 @@ RSpec.describe MerchantFacade do
       end
     end
     
+    context '#merchant_items(merchant_id)' do 
+      it 'creates Item objects for all merchant_items' do
+        merchant = MerchantFacade.merchant_info(5)
+        items = MerchantFacade.merchant_items(5)
+        first_item = items.first
+        expect(merchant).to be_a Merchant
+
+        expect(first_item.merchant_id.to_s).to eq(merchant.id)
+
+        expect(first_item.id).to be_a String
+        expect(first_item.name).to be_a String        
+        expect(first_item.description).to be_a String        
+        expect(first_item.unit_price).to be_a Float
+      end
+    end
   end
 end 
