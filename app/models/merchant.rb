785ac2926aaec2eb93433d6883c5abc8a9e50c9e
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-04 14:51:35 -0400

    Remove: Item & Merchant model/migration

--- app/models/merchant.rb
+++
@@ -1,2 +0,0 @@
-class Merchant < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_170945) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "unit_price"
-    t.bigint "merchant_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["merchant_id"], name: "index_items_on_merchant_id"
-  end
-
-  create_table "merchants", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "items", "merchants"
 end
