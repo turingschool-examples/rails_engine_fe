7b706a603aa449595ac102e6bee9423c0e1bde17
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 12:34:59 -0500

    Add Item poro to store merchant items from API

---
+++ app/poros/item.rb
@@ -0,0 +1,10 @@
+class Item
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:attributes][:name]
+    @description = data[:attributes][:description]
+    @unit_price = data[:attributes][:unit_price]
+    @merchant_id = data[:attributes][:merchant_id]
+  end
+end
