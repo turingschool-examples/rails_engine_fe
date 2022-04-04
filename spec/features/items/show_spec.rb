8cc33d3734841956e92f1812d578905cc76aba57
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 14:53:27 -0500

    Test: item show page has link to merchant show page

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -5,13 +5,22 @@ RSpec.describe 'Item Show Page' do
     first_item = ItemFacade.all_items.first 
 
     visit item_path(first_item.id)
-
+    
     expect(page).to have_content(first_item.name)
     expect(page).to have_content(first_item.description)
     expect(page).to have_content(first_item.unit_price)
   end
   
-  xit 'has link to merchant show page for that item' do 
-    
+  it 'has link to merchant show page for that item' do 
+    first_item = ItemFacade.all_items.first
+    merchant = ItemFacade.item_merchant(first_item.id)
+  
+    visit item_path(first_item.id)
+
+    expect(page).to have_link(merchant.name)
+
+    click_link merchant.name 
+
+    expect(current_path).to eq(merchant_path(merchant.id))
   end
 end
