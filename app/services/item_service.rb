3d5709f1727c1d6a0c0b14cd5e7841aa35b406ab
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-04 14:17:36 -0600

    test: write test for itemservice class method one_item. Define method pass test:

---
+++ app/services/item_service.rb
@@ -0,0 +1,19 @@
+class ItemService 
+
+  def self.get_url(url, param = nil)
+    conn = Faraday.new(url: "http://localhost:3000/") do |faraday|
+      faraday.params[:query] = param unless param == nil
+    end 
+
+  response = conn.get(url)
+  JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.one_item(item_id)
+    get_url("api/v1/items/#{item_id}")[:data]
+  end 
+
+  # def self.all_items
+  #   get_url("api/v1/items")
+  # end 
+end
