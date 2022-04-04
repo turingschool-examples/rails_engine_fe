3db8e0324205cf1787e6c4284c6195656a65486b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-04 17:08:08 -0500

    Test: merchant name search redirects to merchants index page

--- spec/features/application/landing_spec.rb
+++ spec/features/application/landing_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Application Landing Page' do
   it 'search bar to search all merchants by name' do
-
     visit '/'
-
+    
     expect(page).to have_css("#merchant_search")
     expect(page).to have_field(:search)
+  end
+  
+  it 'user can search by merchant name in search bar' do 
+    visit '/'
+
+    fill_in :search, with: 'the'
+    click_button 'Submit'
+
+    expect(current_path).to eq merchants_path
   end
 end
