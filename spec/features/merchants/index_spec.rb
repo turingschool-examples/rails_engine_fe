c6212729fbbc94fe0c64ebb4dc3c2da9f4a0c818
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-04 12:04:17 -0600

    Add: Merchant specs

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants Index Page', type: :feature do
+  it "lists every merchant" do
+    visit "/merchants"
+
+    expect(page).to have_content("Schroeder-Jerde")
+    expect(page).to have_content("Klein, Rempel and Jones")
+    expect(page).to have_content("Willms and Sons")
+    expect(page).to have_content("Cummings-Thiel")
+  end
+
+  it "links to each merchant's show page" do
+    visit "/mercants"
+
+    click_link("Klein, Rempel and Jones")
+    expect(curren_path).to eq("/merchants/2")
+  end
+end
---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Show Page', type: :feature do
+  it "shows the merchant's name" do
+    visit "/merchants/3"
+    expect(page).to have_content("Willms and Sons")
+  end
+
+  it "shows items belonging to the merchant" do
+    visit "/merchants/3"
+    expect(page).to have_content("Item Iusto Atque")
+    expect(page).to have_content("538.98")
+  end
+
+  it "has a link to each item's show page" do
+    visit "/merchants/3"
+    click_link("Item Iusto Atque")
+    expect(current_path).to eq("/items/56")
+  end
+end
