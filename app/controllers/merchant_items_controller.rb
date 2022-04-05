667bfba70f1ef81a0c6b7ef4df6b505a89c30888
Joseph W Galvin <jwgalvin@gmail.com>
2022-04-04 18:08:28 -0600

    started extension. finish later.

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,5 +1,8 @@
-class MerchantItemsController << ApplicationController
+class MerchantItemsController < ApplicationController
   def show
     merch_items = MerchantFacade.merch_items(params[:id])
+  end
+  def index
+    @merch_items = MerchantFacade.merch_items(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants, only: [:index, :show] do
-    resources :merchants_items, only: [:show]
-  end
+  resources :merchants, only: [:index, :show]
+  resources :items, only: [:index, :show], controller: :merchant_items
 end
