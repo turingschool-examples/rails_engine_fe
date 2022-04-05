639ba4b16ac308af48f45308afe10504ce9d42c9
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-04 18:41:08 -0600

    Add: merchant and item routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :merchants, only: [:index, :show]
+  resources :items, only: [:show]
+  namespace :merchants do
+    get ':id/items', to: 'items#index'
+  end
 end
