47bb10ef829b2d0eb37c03302bfca8ee3ed3801e
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-04 19:36:15 -0500

    test: returns one merchant and returns a all of a merchant's items

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 RSpec.describe 'Merchant index page' do
   it 'will list all merchants' do
-    visit '/merchants/'
+    first_merchant = MerchantFacade.all_merchants.first
+
+    visit merchants_path
+    within "#merchants"
     expect(page).to have_content('All Merchants')
-    expect(page).to have_content(@merchants)
+    within  "#index_id_1"
+    expect(page).to have_content(first_merchant.name)
+    expect(page).to have_link(first_merchant.name)
+    click_link(first_merchant.name)
+    expect(current_path).to eq(merchant_path(first_merchant.id))
+    # within  "#index_id_2"
+    # expect(page).to have_content('Klein, Rempel and Jones')
+    # expect(page).to have_link('Klein, Rempel and Jones')
   end
-end
+
+  # it 'links route to merchant show page' do
+    # first_merchant = MerchantFacade.all_merchants.first
+    #
+    # visit merchants_path
+    # within  "#index_id_1" do
+    #   click_link '#{first_merchant.name}'
+    # end
+    # expect(current_path).to eq(merchant_path(first_merchant.id))
+    # end
+  end
