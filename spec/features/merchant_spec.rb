70b72fa09d3c5dcb6df092639e5d0e8d209fe2e9
Luke Pascale <lpluke5@gmail.com>
2022-05-25 23:40:27 -0500

    InitialCommit

---
+++ spec/features/merchant_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'The merchant index page' do
+  it 'has a route of /merchants'
+  it 'displays a list of merchants by name'
+  it 'names that link to merchant show page'
+  it 'has a list of items the merchant sells on the merchants show page'
+end
