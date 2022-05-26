af6e868954c709f700223661bca5741e4b9dfa7d
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-25 21:17:40 -0600

    Test: MerchantFacade.get_merchant_items(merchant_id) returns related items as JSON

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -41,5 +41,33 @@ RSpec.describe MerchantService do
       expect(merchant[:data][:attributes]).to have_key :name
       expect(merchant[:data][:attributes][:name]).to eq('Schroeder-Jerde')
     end
+
+    it '.get_merchant_items(merchant_id) returns all items related to the merchant as JSON' do
+      items = MerchantService.get_merchant_items(1)
+
+      expect(items).to have_key :data
+      expect(items[:data]).to be_an Array
+      expect(items[:data].count).to eq 11
+
+      items[:data].each do |item|
+        expect(item).to have_key :id
+        expect(item[:id]).to be_a String
+
+        expect(item).to have_key :attributes
+        expect(item[:attributes]).to be_a Hash
+
+        expect(item[:attributes]).to have_key :name
+        expect(item[:attributes][:name]).to be_a String
+
+        expect(item[:attributes]).to have_key :description
+        expect(item[:attributes][:description]).to be_a String
+
+        expect(item[:attributes]).to have_key :unit_price
+        expect(item[:attributes][:unit_price]).to be_a Float
+
+        expect(item[:attributes]).to have_key :merchant_id
+        expect(item[:attributes][:merchant_id]).to eq 1
+      end
+    end
   end
 end
