21e9f2d1c67f9a0713f1233a749cb534b1cb70af
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-25 21:04:22 -0600

    Test: the merchants show page should display all items related to the merchant

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -9,4 +9,30 @@ RSpec.describe 'The Merchant Show page' do
     expect(page).to have_content 'Merchant 1'
     expect(page).to have_content 'Name: Schroeder-Jerde'
   end
+
+  it 'displays all items related to the merchant' do
+    stub_merchant_show
+
+    visit '/merchants/1'
+
+    expect(page).to have_css('.item', count: 11)
+
+    within '#item-4' do
+      expect(page).to have_content 'Item Nemo Facere'
+      expect(page).to have_content 'Description: Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.'
+      expect(page).to have_content 'Unit Price: 42.91'
+    end
+
+    within '#item-5' do 
+      expect(page).to have_content 'Item Expedita Aliqua'
+      expect(page).to have_content 'Description: Voluptate aut labore qui illum tempore eius. Corrupti cum et rerum. Enim illum labore voluptatem dicta consequatur. Consequatur sunt consequuntur ut officiis.' 
+      expect(page).to have_content 'Unit Price: 687.23'
+    end
+
+    within '#item-6' do
+      expect(page).to have_content 'Item Provident At'
+      expect(page).to have_content 'Description: Numquam officiis reprehenderit eum ratione neque tenetur. Officia aut repudiandae eum at ipsum doloribus. Iure minus itaque similique. Ratione dicta alias asperiores minima ducimus nesciunt at.'
+      expect(page).to have_content 'Unit Price: 159.25'
+    end
+  end
 end
