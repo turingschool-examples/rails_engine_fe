32a5c1605f7a7f750d21abb133c1ef388ad86d62
Andrew Speth <speth.andrew@yahoo.com>
2022-06-09 19:25:10 -0600

    add bootstrap to views

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,26 +1,20 @@
 class MerchantFacade
-  # def service
-  #   @_service ||= MerchantService.new
-  # end
+  def self.service
+    @_service ||= MerchantService.new
+  end
 
   def self.get_all_merchants
-    service = MerchantService.new
-    
     service.get_all_merchants.map do |merchant|
       Merchant.new(merchant)
     end
   end
   
   def self.find_by_id(id)
-    service = MerchantService.new
-    
     merchant = service.find_by_id(id)
     Merchant.new(merchant)
   end
   
   def self.merchant_items(id)
-    service = MerchantService.new
-
     service.merchant_items(id).map do |item|
       Item.new(item)
     end
