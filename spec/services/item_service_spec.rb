718c50728d7257b2de7af06a8544bf1cf25f9a88
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-20 17:19:20 -0600

    Spec: added item serice spec

--- spec/services/item_service_spec.rb
+++ spec/services/item_service_spec.rb
@@ -3,10 +3,9 @@ require 'rails_helper'
 RSpec.describe 'MerchantService', :vcr do
   it 'retrieves search data and parses response', :vcr do
 
-    parsed_json = MerchantService.all_merchants
-    
+    parsed_json = ItemService.merchant_items(1)
     expect(parsed_json).to be_a Array
     expect(parsed_json[0]).to be_a Hash
-    expect(parsed_json[0][:type]).to eq('merchant')
+    expect(parsed_json[0][:type]).to eq('item')
   end
 end
