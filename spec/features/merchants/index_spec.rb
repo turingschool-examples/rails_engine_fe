5dee0da76988d75c1854fb86d25f6b2c58696424
Gauri Joshi <gauri728@gmail.com>
2022-09-08 13:25:49 -0700

    merchant name is a link

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe "Merchants Index", :vcr do 
   it "has a list of all the Merchants names" do 
     visit "/merchants"
-
     expect(page).to have_content("Schroeder-Jerde")
+    expect(page).to have_content(1)
+  end
+
+  it "links to each merchant's show page", :vcr do 
+    visit "/merchants"
+
+    click_link("Schroeder-Jerde")
+
+    expect(page).to have_link("Schroeder-Jerde")
   end
 end
