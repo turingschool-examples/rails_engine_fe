cd4a333a255abd6dbc9e001c61e4b8f47e391ce4
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-08 11:14:17 -0600

    Feat: create Faraday connection and parse response from ::get_merchant

---
+++ app/services/merchant_service.rb
@@ -0,0 +1,11 @@
+class MerchantService
+
+  def self.conn
+    Faraday.new(url: 'http://localhost:3000')
+  end
+
+  def self.get_merchant(merchant_id)
+    response = conn.get('/api/v1/merchants')
+    JSON.parse(response.body, symbolize_names: true)[:data][0]
+  end
+end
