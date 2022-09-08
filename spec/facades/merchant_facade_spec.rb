6d400f5ee3fe1be075a29db898d4251022453541
Gauri Joshi <gauri728@gmail.com>
2022-09-08 14:45:06 -0700

    test: merchant facade specific merchant

--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe MerchantFacade do
     expect(merchants[0]).to be_a(Merchant)
     expect(merchants).to be_a(Array)
   end
+
+  it "gets a specific merchant", :vcr do 
+    merchant = MerchantFacade.create_merchant_search(1)
+    expect(merchant).to be_a(Merchant)
+    expect(merchant.id).to eq("1")
+    expect(merchant.name).to eq("Schroeder-Jerde")
+  end
 end
