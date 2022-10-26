90c0f0773214a24653f5b73a954f647d7c6b647d
dominicod <dominicodonnell99@gmail.com>
2022-10-26 14:27:03 -0500

    Test: write tests for engine_service

--- spec/services/engine_service_spec.rb
+++ spec/services/engine_service_spec.rb
@@ -23,6 +23,26 @@ RSpec.describe EngineService, type: :service do
         merchant_attributes(response[:data])
       end
     end
+
+    describe '.items' do
+      it 'returns an index of all Items' do
+        response = EngineService.items
+
+        expect(response[:data]).to be_an Array
+        response[:data].each do |item|
+          item_attributes(item)
+        end
+      end
+    end
+
+    describe '.item(:id)' do
+      it 'returns a single ItemObject' do
+        response = EngineService.item(1)
+
+        expect(response[:data]).to be_an Hash
+        item_attributes(response[:data])
+      end
+    end
   end
 end
 
@@ -31,4 +51,13 @@ def merchant_attributes(merchant)
   expect(merchant[:type]).to eq 'merchant'
   expect(merchant[:attributes]).to be_an Hash
   expect(merchant.dig(:attributes, :name)).to be_an String
+end
+
+def item_attributes(item)
+  expect(item[:id]).to be_an String
+  expect(item[:type]).to eq 'item'
+  expect(item[:attributes]).to be_an Hash
+  expect(item.dig(:attributes, :name)).to be_an String
+  expect(item.dig(:attributes, :description)).to be_an String
+  expect(item.dig(:attributes, :unit_price)).to be_an Float
 end
