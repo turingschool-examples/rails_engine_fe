0f4986d1f33bfaef0b95f82569e49a1eeedee144
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-26 12:24:09 -0600

    test: add skeleton tests for index and show spec.

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'The Merchant Index Page' do
+ it 'returns a list of merchants' do
+
+ end
+
+ describe "As a visitor, When I visit '/merchants'" do
+   it "I should see a list of merchants by name" do
+
+   end
+ end
+
+ describe "As a visitor, When I visit '/merchants'" do
+  it "When I click the merchant's name I should be on page '/merchants/:id'" do
+
+  end
+ end
+end
---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'The Merchant Show Page' do
+ describe 'returns a list of items per merchant' do
+  describe "As a visitor, When I visit '/merchants/:id'" do
+   it "I should see the name of that merchant" do
+
+   end
+  end
+ end
+
+ describe "As a visitor, When I visit '/merchants/:id'" do
+  it "I should see a list of items that merchant sells." do
+
+  end
+ end
+end
