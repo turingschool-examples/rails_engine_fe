6c7fc938b483c6623a63e43c1d4de11f3b37e4ac
dominicod <dominicodonnell99@gmail.com>
2022-10-26 16:27:34 -0500

    Feat: add launchy to gemfile, Test: create test for item index

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   gem 'simplecov'
   gem 'vcr'
   gem 'webmock'
+  gem 'launchy'
 end
 
 group :development do
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -4,6 +4,25 @@ require 'rails_helper'
 
 RSpec.describe 'Items | Index', :vcr, type: :feature do
   describe 'As a visitor, when I visit /items;' do
+    before(:each) { visit items_path }
+    items = { 'Item Nemo Facere': 4, 'Item Expedita Aliquam': 5, 'Item Est Consequuntur': 8, 'Item Quo Magnam': 9,
+              'Item Quidem Suscipit': 10, 'Item Provident At': 6 }
+    it 'I should see a list of items' do
+      within('#items') do
+        items.each { |item_name, _id| expect(page).to have_content item_name }
+      end
+    end
 
+    it 'each item should have a link to its show page' do
+      within('#items') do
+        items.each do |item_name, id|
+          click_on item_name
+
+          expect(current_path).to eq item_path(id)
+
+          visit items_path
+        end
+      end
+    end
   end
 end
