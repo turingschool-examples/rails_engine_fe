264ac1aaf78020338ad539fb19b8738b8b19f571
Carter Ball <carter.k.ball@gmail.com>
2022-10-26 13:47:03 -0400

    Request all merchants in MerchantService working

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -2,4 +2,8 @@ require 'faraday'
 
 class MerchantService
   
+  def self.request_all_merchants
+    response = Faraday.get("http://localhost:3000/api/v1/merchants")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe MerchantService do
   describe 'class methods' do
     it 'returns a list/data about all merchants' do
       all_merchant_data = MerchantService.request_all_merchants
-
+      all_merchants = all_merchant_data[:data] 
       expect(all_merchant_data).to be_a(Hash)
+      expect(all_merchants).to be_a(Array)
+      all_merchants.each do |merchant|
+        expect(merchant[:type]).to eq("merchant")
+        expect(merchant[:id].to_i).to be_a(Integer)
+        expect(merchant[:attributes]).to have_key(:name)
+      end
     end
 
     it 'returns info about a single merchant' do
