a235894ec8d86fbd6a57513bdce9cc37e8fce926
dominicod <dominicodonnell99@gmail.com>
2022-10-26 13:15:39 -0500

    Refactor: index into the response with [:data] and send that as an argument

--- app/facades/engine_facade.rb
+++ app/facades/engine_facade.rb
@@ -3,13 +3,13 @@
 class EngineFacade
   def self.create_merchants
     merchants = EngineService.merchants
-    merchants.map do |merchant_data|
+    merchants[:data].map do |merchant_data|
       MerchantObject.new(merchant_data)
     end
   end
 
   def self.create_merchant(id)
     merchant_data = EngineService.merchant(id)
-    MerchantObject.new(merchant_data)
+    MerchantObject.new(merchant_data[:data])
   end
 end
