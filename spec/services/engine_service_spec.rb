b940cbb2d2222011567219451e5278b818dd349d
dominicod <dominicodonnell99@gmail.com>
2022-10-26 13:00:12 -0500

    Test: create passing test for single merchant API call

--- spec/services/engine_service_spec.rb
+++ spec/services/engine_service_spec.rb
@@ -2,20 +2,33 @@
 
 require 'rails_helper'
 
-RSpec.describe EngineService, :vcr, type: :service do
-  describe 'Class Methods' do
+RSpec.describe EngineService, type: :service do
+  describe 'Class Methods', :vcr do
     describe '.merchants' do
       it 'returns an index of all Merchants' do
         response = EngineService.merchants
 
         expect(response[:data]).to be_an Array
         response[:data].each do |merchant|
-          expect(merchant[:id]).to be_an String
-          expect(merchant[:type]).to eq 'merchant'
-          expect(merchant[:attributes]).to be_an Hash
-          expect(merchant.dig(:attributes, :name)).to be_an String
+          merchant_attributes(merchant)
         end
       end
     end
+
+    describe '.merchant(:id)' do
+      it 'returns a single Merchant' do
+        response = EngineService.merchant(1)
+
+        expect(response[:data]).to be_an Hash
+        merchant_attributes(response[:data])
+      end
+    end
   end
+end
+
+def merchant_attributes(merchant)
+  expect(merchant[:id]).to be_an String
+  expect(merchant[:type]).to eq 'merchant'
+  expect(merchant[:attributes]).to be_an Hash
+  expect(merchant.dig(:attributes, :name)).to be_an String
 end
