14a178b62ac67964301f64b20b7fc4a5e87d071c
Carter Ball <carter.k.ball@gmail.com>
2022-10-26 17:14:35 -0400

    Merchants index page displaying all names

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,6 @@
 class MerchantsController < ApplicationController
 
   def index
-    
+    @merchants = MerchantFacade.find_merchants
   end
 end
--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'the Merchant Index Page' do
   before :each do
-    visit "/merchants"
+    visit merchants_path
   end
 
-  it 'exists' do
+  it 'displays all merchant names' do
     
+    
+    expect(page).to have_content("Name: Tromp Inc")
+    expect(page).to have_content("Name: Williamson Group")
   end
 end
