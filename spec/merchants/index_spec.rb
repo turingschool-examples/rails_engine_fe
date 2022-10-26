8b3a2116919a45659d7800e71356ef110d9820b7
dominicod <dominicodonnell99@gmail.com>
2022-10-26 12:17:19 -0500

    Test: fleshout merchants tests

--- spec/merchants/index_spec.rb
+++ spec/merchants/index_spec.rb
@@ -2,6 +2,28 @@
 
 RSpec.describe 'Merchants | Index', :vcr, type: :feature do
   describe 'As a visitor, when I visit /merchants;' do
+    let!(:visit) { visit merchants_path }
+    merchant_list = { 'Schroeder-Jerde': 1, 'Klein, Rempel and Jones': 2, 'Willms and Sons': 3, 'Cummings-Thiel': 4,
+                      'Williamson Group': 6, 'Bechtelar, Jones and Stokes': 8 }
 
+    it 'I should see a list of merchants by name' do
+      within('#merchants') do
+        within('h1') do
+          expect(page).to have_content('Merchants')
+        end
+
+        merchant_list.each { |merchant_name, _id| expect(page).to have_content merchant_name }
+      end
+    end
+
+    it 'each Merchant should have a link to its show page' do
+      merchant_list.each do |merchant_name, id|
+        click_on merchant_name
+
+        expect(current_path).to eq merchant_path(id)
+
+        visit merchants_path
+      end
+    end
   end
 end
--- spec/merchants/show_spec.rb
+++ spec/merchants/show_spec.rb
@@ -2,6 +2,14 @@
 
 RSpec.describe 'Merchants | Show', :vcr, type: :feature do
   describe 'As a visitor, when I visit /merchant/:id;' do
+    let!(:visit) { visit merchant_path(1) }
 
+    it 'I should see a list of items that merchant sells' do
+      items = { 'Item Nemo Facere': 4, 'Item Expedita Aliquam': 5, 'Item Est Consequuntur': 8, 'Item Quo Magnam': 9,
+                'Item Quidem Suscipit': 10, 'Item Rerum Magni': 11 }
+      within('#items') do
+        items.each { |item_name, _id| expect(page).to have_content item_name }
+      end
+    end
   end
 end
