7a68835e3f8c8f6f536bb340399ccef2c8bedc60
Carter Ball <carter.k.ball@gmail.com>
2022-10-26 14:46:18 -0400

    Fill out merchant poro tests

--- spec/poros/merchant_poro_spec.rb
+++ spec/poros/merchant_poro_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Merhcant do
+RSpec.describe Merchant do
   it 'exists and displays attributes correctly' do
-    merchant_data = MerchantService.request_one_merchant(3)
+    raw_merchant_data = MerchantService.request_one_merchant(3)
+    merchant_data = raw_merchant_data[:data]
     merchant = Merchant.new(merchant_data)
+
+    expect(merchant).to be_instance_of(Merchant)
+    expect(merchant.id.to_i).to eq(3)
+    expect(merchant.name).to eq("Willms and Sons")
   end
 end
