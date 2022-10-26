908d7d323736d3e36ccc3c2ff7e3c901250579bf
dominicod <dominicodonnell99@gmail.com>
2022-10-26 12:19:55 -0500

    Test: add more to tests

--- spec/merchants/show_spec.rb
+++ spec/merchants/show_spec.rb
@@ -3,12 +3,24 @@
 RSpec.describe 'Merchants | Show', :vcr, type: :feature do
   describe 'As a visitor, when I visit /merchant/:id;' do
     let!(:visit) { visit merchant_path(1) }
+    items = { 'Item Nemo Facere': 4, 'Item Expedita Aliquam': 5, 'Item Est Consequuntur': 8, 'Item Quo Magnam': 9,
+              'Item Quidem Suscipit': 10, 'Item Rerum Magni': 11 }
 
     it 'I should see a list of items that merchant sells' do
-      items = { 'Item Nemo Facere': 4, 'Item Expedita Aliquam': 5, 'Item Est Consequuntur': 8, 'Item Quo Magnam': 9,
-                'Item Quidem Suscipit': 10, 'Item Rerum Magni': 11 }
       within('#items') do
         items.each { |item_name, _id| expect(page).to have_content item_name }
+      end
+    end
+
+    it 'each Item should have a link to its show page' do
+      within('#items') do
+        items.each do |item_name, id|
+          click_on item_name
+
+          expect(current_path).to eq item_path(id)
+
+          visit merchant_path(1)
+        end
       end
     end
   end
