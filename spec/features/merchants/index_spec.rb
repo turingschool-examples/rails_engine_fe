373e6c9eed6bd6c37ec5daf749e9b769ff003ce7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-26 12:33:04 -0600

    test: add complete skeleton tests for soa challenge

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'The Merchant Index Page' do
  it 'returns a list of merchants' do
+  visit merchants_path
 
+  expect(page).to have_content([merchant1, merchant2, merchant3])
  end
-
+ 
  describe "As a visitor, When I visit '/merchants'" do
-   it "I should see a list of merchants by name" do
+  it "I should see a list of merchants by name" do
+   visit merchants_path
 
+   within("#merchants") do
+    expect(page).to have_content(merchant.name)
    end
+  end
  end
 
  describe "As a visitor, When I visit '/merchants'" do
   it "When I click the merchant's name I should be on page '/merchants/:id'" do
+   visit merchants_path
 
+   within("#merchants") do
+    expect(page).to have_link(merchant.name)
+
+    click_link "Merchant Name"
+
+    expect(current_path).to eq(merchant_path(merchant.id))
+   end
   end
  end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,14 +4,20 @@ RSpec.describe 'The Merchant Show Page' do
  describe 'returns a list of items per merchant' do
   describe "As a visitor, When I visit '/merchants/:id'" do
    it "I should see the name of that merchant" do
+    visit merchants_path(merchant.id)
 
+    expect(page).to have_content(merchant.name)
    end
   end
  end
 
  describe "As a visitor, When I visit '/merchants/:id'" do
   it "I should see a list of items that merchant sells." do
+   visit merchants_path(merchant.id)
 
+   within("#items") do
+    expect(page).to have_link(item.name)
+   end
   end
  end
 end
