75caa4b2d9f7e2ce80523214172126033b8a8e81
dominicod <dominicodonnell99@gmail.com>
2022-10-26 16:32:04 -0500

    Test: create passing test for item show

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -4,6 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'Items | Show', :vcr, type: :feature do
   describe 'As a visitor, when I visit /items/:id;' do
-
+    before(:each) { visit item_path(4) }
+    it 'I should see the name of the item, description, and unit price.' do
+      expect(page).to have_content 'Item Nemo Facere'
+      expect(page).to have_content 'Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.'
+      expect(page).to have_content '42.91'
+    end
   end
 end
