d87ebaa048e268181e84450e2172d6b8ea888801
dominicod <dominicodonnell99@gmail.com>
2022-10-26 12:29:21 -0500

    Feat: setup service logic

--- app/services/engine_service.rb
+++ app/services/engine_service.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class EngineService
-  def self.merchants; end
+  def self.merchants
+    parse(conn.get('/merchants'))
+  end
+
+  def self.conn
+    con ||= Faraday.new('http://localhost:3000/api/v1/')
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
