f316ba05c6d7f7ed0c8c97899fc7c0889d02e867
Carter Ball <carter.k.ball@gmail.com>
2022-10-26 14:01:28 -0400

    MerchantService returning info for one merchant and tests

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -6,4 +6,9 @@ class MerchantService
     response = Faraday.get("http://localhost:3000/api/v1/merchants")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.request_one_merchant(id)
+    response = Faraday.get("http://localhost:3000/api/v1/merchants/#{id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -11,11 +11,19 @@ RSpec.describe MerchantService do
         expect(merchant[:type]).to eq("merchant")
         expect(merchant[:id].to_i).to be_a(Integer)
         expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant.count).to eq(3)
       end
     end
 
     it 'returns info about a single merchant' do
-
+      one_merchant_data = MerchantService.request_one_merchant(3)
+      one_merchant = one_merchant_data[:data]
+      
+      expect(one_merchant_data).to be_a(Hash)
+      expect(one_merchant).to be_a(Hash)
+      expect(one_merchant.count).to eq(3)
+      expect(one_merchant[:type]).to eq("merchant")
+      expect(one_merchant[:attributes]).to have_key(:name)
     end
   end
 end
