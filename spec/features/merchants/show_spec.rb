fbad6fac200aec87f5c9322ea5bf2f00bf10dd40
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-27 10:00:01 -0600

    test: skeleton test for merchant items,

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -15,9 +15,7 @@ RSpec.describe 'The Merchant Show Page' do
   it "I should see a list of items that merchant sells." do
    visit "/merchants/1"
 
-   # within("#items") do
-   #  expect(page).to have_link(item.name)
-   # end
+   expect(page).to have_content(item.name)
   end
  end
 end
