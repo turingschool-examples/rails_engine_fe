032fbc346af241136f274a9af46bedfb03dfda1b
Thomas Musselman <tmusselman@gmail.com>
2022-10-27 12:03:59 -0600

    feat: add merchant relationship for item

--- app/poros/item.rb
+++ app/poros/item.rb
@@ -6,5 +6,10 @@ class Item
     @name = item_data[:attributes][:name]
     @description = item_data[:attributes][:description]
     @unit_price = item_data[:attributes][:unit_price]
+    @merchant_id = item_data[:attributes][:merchant_id]
+  end
+
+  def merchant
+    MerchantsFacade.get_merchant(@merchant_id)
   end
 end
