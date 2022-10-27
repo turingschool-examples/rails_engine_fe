9e37c6c824a030c9e3fb66bfbbdae3f549e664ad
Thomas Musselman <tmusselman@gmail.com>
2022-10-27 10:39:52 -0600

    test: merchants show page has merchant items

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe "merchants show page" do
 
   before :each do
     @merchant = MerchantsFacade.get_merchants.first
+    @merchant_items = ItemsFacade.get_merchant_items(@merchant.id)
     visit merchant_path(@merchant.id)
   end
 
   it 'shows the merchants name' do
     expect(page).to have_content(@merchant.name)
+  end
+
+  it 'shows the merchant items' do
+    @merchant_items.each do |item|
+      expect(page).to have_content(item.name)
+    end
   end
 end
