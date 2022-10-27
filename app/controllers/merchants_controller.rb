8287edc7ab128e89347e55309a92ab26a2bdb34f
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-27 10:54:41 -0600

    feat: passing merchant retrieval for us1 along with link to merchants show page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,11 @@
 class MerchantsController <ApplicationController
   def index
+    response = ::Faraday.get('http://localhost:3000/api/v1/merchants')
+    @merchants = JSON.parse(response.body, symbolize_names: true)[:data] #returning an array of merchant data
+  end
 
+  def show
+    response = Faraday.get("http://localhost:3000/api/v1/merchants/#{params[:id]}") #refactor to a service
+    @merchant = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -11,7 +11,13 @@ RSpec. describe 'merchants index' do
   it 'retrieves a list of merchants' do
     visit merchants_path
     expect(page).to have_content('Merchants')
+    expect(page).to have_content('Schroeder-Jerde')
+
+    click_on 'Schroeder-Jerde'
+    expect(current_path).to eq('/merchants/1')
   end
+
+
 
 
 end
