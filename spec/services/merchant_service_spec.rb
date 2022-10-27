d0d0c74370f4daa671e47e7b90928035b00a38fc
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-27 11:01:47 -0400

    merchant service test 2

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -20,5 +20,30 @@ describe MerchantService do
         expect(merchant[:attributes][:name]).to be_a(String)
       end
     end
+
+    context "#get_merchants_items(id)" do
+      it "returns a merchants items & data about those items", :vcr do
+        json_merchant_items = MerchantService.get_merchants_items(1)
+
+        expect(json_merchant_items).to be_a Hash
+        expect(json_merchant_items[:data]).to be_an Array
+        item = json_merchant_items[:data].first
+
+        expect(item).to have_key :id
+        expect(item[:id]).to be_a(String)
+
+        expect(item).to have_key :attributes
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key :description
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key :unit_price
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+
+        expect(item[:attributes]).to have_key :merchant_id
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
+    end
   end
 end
