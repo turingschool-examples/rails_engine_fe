93a693881661c094b6f15de82658d7d81e77f792
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-27 10:21:56 -0600

    feat: add updated specs and complete merchant item api

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'The Merchant Show Page' do
     visit "/merchants/1"
 
     expect(page).to have_content("Schroeder-Jerde")
+    expect(page).to_not have_content("Klein, Rempel and Jones")
+    expect(page).to_not have_content("Williamson Group")
    end
   end
  end
@@ -15,7 +17,12 @@ RSpec.describe 'The Merchant Show Page' do
   it "I should see a list of items that merchant sells." do
    visit "/merchants/1"
 
-   expect(page).to have_content(item.name)
+   expect(page).to have_content('Item Nemo Facere')
+   expect(page).to have_content('Item Expedita Aliquam')
+   expect(page).to have_content('Item Provident At')
+   expect(page).to have_content('Item Expedita Fuga')
+   expect(page).to_not have_content('Item Non In')
+   expect(page).to_not have_content('Item Et Sequi')
   end
  end
 end
