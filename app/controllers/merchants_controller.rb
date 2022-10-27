56faec34088c1171d7804c0afeeb901dd53243bb
Carter Ball <carter.k.ball@gmail.com>
2022-10-27 12:03:35 -0400

    Merchant show page exists

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,4 +3,8 @@ class MerchantsController < ApplicationController
   def index
     @merchants = MerchantFacade.find_merchants
   end
+
+  def show
+    
+  end
 end
---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'the Merchant Show Page' do
+  before :each do
+    visit merchant_path(1)
+  end
+
+  it 'exists' do
+
+  end
+
+end
