26a1fdb5fa4b2519571e2f9a7c4c4a73ba6e22ad
Thomas Musselman <tmusselman@gmail.com>
2022-10-27 10:22:43 -0600

    test: merchants name is link to show page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe "merchants index page" do
       expect(page).to have_content(merchant.name)
     end
   end
+
+  it 'each merchant name is a link to their show page' do
+    visit merchants_path
+
+    @merchants.each do |merchant|
+      expect(page).to have_link(merchant.name)
+    end
+
+    click_link @merchants.first.name
+    
+    expect(current_path).to eq(merchant_path(@merchants.first.id))
+  end
 end
