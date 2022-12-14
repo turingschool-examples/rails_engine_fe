c5ba863d019b792f1b3617b0e2b15d2f4b6893e8
Darby Smith <newman.darby2@gmail.com>
2022-12-14 15:15:52 -0500

    Test for merchant index

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'merchants index page' do
+  it 'shows all merchants by name' do
+    visit merchants_path
+
+    expect(page).to have_content("Schroeder-Jerde")
+    expect(page).to have_content("Klein, Rempel and Jones")
+    expect(page).to have_content("Willms and Sons")
+    expect(page).to_not have_content("Item Nemo Facere")
+  end 
+
+  it 'shows the merchants name as a link that goes to the merchant show page' do
+    visit merchants_path
+
+    expect(page).to have_content("Schroeder-Jerde")
+    click_on "Schroeder-Jerde"
+
+    expect(current_path).to eq(merchant_path(1))
+
+    within("#items") do
+      expect(page).to have_content("Item Nemo Facere")
+      expect(page).to have_content("Item Expedita Aliquam")
+      expect(page).to have_content("Item Provident At")
+      expect(page).to_not have_content("Item Adipisci Sint")
+    end
+  end
+end
