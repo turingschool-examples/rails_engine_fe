654fea8881a6a4e3ef07f21087e5192291d1f637
Amanda Ross <amikaross@gmail.com>
2022-12-14 11:43:35 -0700

    Remove save and open page from index_spec

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "the merchants index page" do
     describe "When I visit '/merchants'" do 
       it "should display a list of merchants by name, each linked to their show page", :vcr do 
         visit merchants_path
-save_and_open_page
+
         expect(page).to have_link("Schroeder-Jerde", href: merchant_path(1))
         expect(page).to have_link("Klein, Rempel and Jones", href: merchant_path(2))
         expect(page).to have_link("Willms and Sons", href: merchant_path(3))
