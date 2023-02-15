6c740048f294e2a79cc51565f5bda84bd6f58a45
Anhnhi Tran <anhtran811@gmail.com>
2023-02-15 16:47:13 -0700

    Add facade for a merchant by id

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,8 +1,13 @@
 class MerchantFacade
-  def self.merchant_data
-    merchants_response = MerchantService.get_merchants
+  def self.all_merchants_data
+    merchants_response = MerchantService.get_all_merchants
     merchants = merchants_response[:data].map do |merchant|
       Merchant.new(merchant)
     end
+  end
+  
+  def self.merchant_data(id)
+    merchant_response = MerchantService.get_merchant(id)[:data]
+    merchant = Merchant.new(merchant_response) 
   end
 end
