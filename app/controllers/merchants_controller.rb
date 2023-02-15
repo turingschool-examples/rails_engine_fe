ffd9853278ea301ef283d0f7d4b0c7b5f377579c
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-15 10:49:18 -0800

    get basic function working

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,9 @@
 class MerchantsController < ApplicationController 
 
   def index 
-    
+    conn = Faraday.new(url: 'http://localhost:3000')
+    response = conn.get("/api/v1/merchants")
+
+    @merchants = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -5,10 +5,20 @@ RSpec.describe 'Merchant Index' do
     it 'shows a list of merchants by name' do 
       visit merchants_path 
       
+      expect(page.status_code).to eq 200
+      expect(page).to have_content("Merchants")
+      expect(page).to have_content("Schroeder-Jerde")
+      expect(page).to have_content("Willms and Sons")
+      expect(page).to have_content("Pollich and Sons")
     end
 
-    xit 'has a link for each merchant name to merchant show page' do 
-
+    it 'has a link for each merchant name to merchant show page' do 
+      visit merchants_path 
+      
+      expect(page.status_code).to eq 200
+      expect(page).to have_link("Schroeder-Jerde")
+      expect(page).to have_link("Willms and Sons")
+      expect(page).to have_link("Pollich and Sons")
     end
   end
 end
