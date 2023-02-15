31e7ad1a859d8a2d8521d6f261da25ce35fb3586
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-15 12:50:59 -0800

    update merchant service to have merchant items call

--- app/services/merchants_service.rb
+++ app/services/merchants_service.rb
@@ -4,6 +4,10 @@ class MerchantsService
     parse_data(conn.get("/api/v1/merchants"))
   end
 
+  def self.get_merchant_items(merchant_id)
+    parse_data(conn.get("/api/v1/merchants/#{merchant_id}/items"))
+  end
+
   def self.parse_data(response)
     JSON.parse(response.body, symbolize_names: true)
   end
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe MerchantsService do
       expect(merchant_result[:attributes]).to have_key(:name)
     end
   end
+
+  it 'can return the merchant items' do 
+    response = MerchantsService.get_merchant_items(1)
+
+    expect(response).to be_a Hash 
+    expect(response).to have_key(:data)
+    expect(response[:data]).to be_an Array 
+
+    response[:data].each do |item_result| 
+      expect(item_result).to have_key(:id)
+      expect(item_result).to have_key(:type)
+      expect(item_result).to have_key(:attributes)
+      expect(item_result[:attributes]).to have_key(:name)
+      expect(item_result[:attributes]).to have_key(:description)
+      expect(item_result[:attributes]).to have_key(:unit_price)
+      expect(item_result[:attributes]).to have_key(:merchant_id)
+    end 
+  end
 end
