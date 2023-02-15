075daf99e2ece42c5fdf563ad23d49b381ca7d55
Anhnhi Tran <anhtran811@gmail.com>
2023-02-15 16:46:11 -0700

    Add method and test for consuming one merchant

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,6 +1,11 @@
 class MerchantService
-  def self.get_merchants
+  def self.get_all_merchants
     response = conn.get('api/v1/merchants')
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_merchant(id)
+    response = conn.get("api/v1/merchants/#{id}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
