9df856da543a315835a6ccfec32db7b69fb84f8a
Anhnhi Tran <anhtran811@gmail.com>
2023-02-15 21:35:51 -0700

    Completed items extension

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Item show page' do
+  it 'can return an item details' do
+    visit item_path(4)
+
+    expect(page).to have_content("Item Nemo Facere")
+    expect(page).to have_content("Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
+    expect(page).to have_content(42.91)
+  end
+end
